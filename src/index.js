2c9c63dc94d18d67a641a3ebcf080c8e2bc49522
Jeong SeongHun <2000tjdgns@naver.com>
2023-07-27 15:58:21 +0900

    ready for useTabs(deleted useInput)

--- src/index.js
+++ src/index.js
@@ -3,30 +3,10 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initalValue, validator) => {
-  const [value, setValue] = useState(initalValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
-
 const App = () => {
-  const maxLen = (value) => !value.includes("@");
-  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
     </div>
   );
 };
