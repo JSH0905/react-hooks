0c740bb3fcddb152a607ce8ebc4a69da7ac075ad
Jeong SeongHun <2000tjdgns@naver.com>
2023-07-27 16:42:14 +0900

    learned about useTabs Hook

--- src/index.js
+++ src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useTabs = (initalTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initalTab);
+
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+
 const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
